538235a0a886c8c6dc4f09c904ef1127b551e28a
Olga Kondratenko <kondratenko666olga@gmail.com>
2024-03-24 19:01:21 +0200

    reinstalled

---
+++ src/js/2-form.js
@@ -0,0 +1,29 @@
+const STORAGE_KEY = "feedback-form-state";
+
+const form = document.querySelector(".feedback-form");
+const textarea = form.querySelector("textarea");
+
+form.addEventListener("submit", handleSubmit);
+textarea.addEventListener("input", onTextareaInput);
+
+populateTextarea();
+
+function handleSubmit(event) {
+    event.preventDefault();
+    console.log("send");
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(event) {
+    const message = event.target.value;
+    localStorage.setItem(STORAGE_KEY, message);
+}
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+    if(savedMessage) {
+        textarea.value = savedMessage;
+    }
+}
